7c6692a00984d75a831446a3093fd73f3e5eff6a
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2021-04-24 19:02:14 -0300

    Conecta la api de movimientos, saca los datos mock

--- client/js/movement-service.js
+++ client/js/movement-service.js
@@ -1,46 +1,9 @@
 const BASE_URL = '/api/v1';
 
-const FAKE = [
-    {
-        date: '05/01/2021',
-        amount: 10000.0,
-        type: 'income',
-        category: 'Plazo Fijo',
-    },
-    {
-        date: '05/01/2021',
-        amount: 10000.0,
-        type: 'income',
-        category: 'Plazo Fijo',
-    },
-    {
-        date: '05/01/2021',
-        amount: 10000.0,
-        type: 'expense',
-        category: 'Plazo Fijo',
-    },
-    {
-        date: '05/01/2021',
-        amount: 10000.0,
-        type: 'income',
-        category: 'Plazo Fijo',
-    },
-    {
-        date: '05/01/2021',
-        amount: 10000.0,
-        type: 'expense',
-        category: 'Plazo Fijo',
-    },
-];
-
 async function getLast(id) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(FAKE);
-        }, 300);
-    });
-    //const resp = await fetch(`${BASE_URL}/movements`);
-    //return resp.json();
+    const resp = await fetch(`${BASE_URL}/movements`);
+    const { movements } = await resp.json();
+    return movements;
 }
 
 export default {
