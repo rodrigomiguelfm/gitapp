17faa5b6da61d56a38241f35dd8519e1a5200a8d
ivanduva <duva.ivan@outlook.com>
2021-04-25 18:57:11 -0300

    Agrega query en el endpoint de listar movimientos para filtrar por tipo

--- server/routes/movement.js
+++ server/routes/movement.js
@@ -3,25 +3,28 @@ const paginate = require('express-paginate');
 const MovementModel = require('../models/movement');
 const router = express.Router();
 
-
 /**
  * Endpoint para obtener todos los movimientos.
  *
  */
 router.get('/', function (req, res) {
-    MovementModel.getAll(req.query.limit, req.skip)
-        .then(results => {
+    MovementModel.getAll(req.query.limit, req.skip, req.query.type)
+        .then((results) => {
             const pageCount = Math.ceil(results.count / req.query.limit);
 
-            const pages = paginate.getArrayPages(req)(3, pageCount, req.query.page)
+            const pages = paginate.getArrayPages(req)(
+                3,
+                pageCount,
+                req.query.page
+            );
 
             res.status(200).send({
                 movements: results.rows,
                 pages: pages,
                 next: paginate.hasNextPages(req)(pageCount)
                     ? pages[req.query.page].url
-                    : null
-            })
+                    : null,
+            });
         })
         .catch((_) => {
             console.log(_);
--- tests/integration/index.test.js
+++ tests/integration/index.test.js
@@ -30,9 +30,9 @@ test('Se debería iniciar la aplicación sin movimientos', async () => {
 test('Obtener movimientos por api', async () => {
     const movementData = {
         date: '04/01/2021',
-        amount: 50000.00,
+        amount: 50000.0,
         type: MovementType.INCOME,
-        category: 'Sueldo'
+        category: 'Sueldo',
     };
 
     // Creamos el movimiento
@@ -48,15 +48,15 @@ test('Obtener movimientos por api', async () => {
 test('Buscar movimientos por api con un resultado', async () => {
     const firstMovementData = {
         date: '01/01/2021',
-        amount: 1000.00,
-        category: 'Supermercado'
+        amount: 1000.0,
+        category: 'Supermercado',
     };
 
     const secondMovementData = {
         date: '04/01/2021',
-        amount: 50000.00,
+        amount: 50000.0,
         type: MovementType.INCOME,
-        category: 'Sueldo'
+        category: 'Sueldo',
     };
 
     // Creamos los movimientos
@@ -71,18 +71,121 @@ test('Buscar movimientos por api con un resultado', async () => {
     expect(firstMovement.id).toBe(response.movements[0].id);
 });
 
-test('Buscar movimientos por api con más de un resultado', async () => {
+test('Buscar movimientos por api con offset', async () => {
     const firstMovementData = {
         date: '01/01/2021',
-        amount: 1000.00,
-        category: 'Supermercado'
+        amount: 1000.0,
+        category: 'Supermercado',
     };
 
     const secondMovementData = {
         date: '04/01/2021',
-        amount: 50000.00,
+        amount: 50000.0,
         type: MovementType.INCOME,
-        category: 'Sueldo'
+        category: 'Sueldo',
+    };
+
+    // Creamos los movimientos
+    await MovementModel.create(firstMovementData);
+    const secondMovement = await MovementModel.create(secondMovementData);
+
+    const URL = `${baseURL}/movements?limit=1&page=2`;
+    const req = await fetch(URL);
+    const response = await req.json();
+
+    expect(response.movements.length).toBe(1);
+    expect(secondMovement.id).toBe(response.movements[0].id);
+});
+
+test('Buscar movimientos por api filtrando por tipo income', async () => {
+    const firstMovementData = {
+        date: '01/01/2021',
+        amount: 1000.0,
+        category: 'Supermercado',
+    };
+
+    const secondMovementData = {
+        date: '04/01/2021',
+        amount: 50000.0,
+        type: MovementType.INCOME,
+        category: 'Sueldo',
+    };
+
+    // Creamos los movimientos
+    await MovementModel.create(firstMovementData);
+    const secondMovement = await MovementModel.create(secondMovementData);
+
+    const URL = `${baseURL}/movements?type=${MovementType.INCOME}`;
+    const req = await fetch(URL);
+    const response = await req.json();
+
+    expect(response.movements.length).toBe(1);
+    expect(secondMovement.id).toBe(response.movements[0].id);
+});
+
+test('Buscar movimientos por api filtrando por tipo expense', async () => {
+    const firstMovementData = {
+        date: '01/01/2021',
+        amount: 1000.0,
+        category: 'Supermercado',
+    };
+
+    const secondMovementData = {
+        date: '04/01/2021',
+        amount: 50000.0,
+        type: MovementType.INCOME,
+        category: 'Sueldo',
+    };
+
+    // Creamos los movimientos
+    const firstMovement = await MovementModel.create(firstMovementData);
+    await MovementModel.create(secondMovementData);
+
+    const URL = `${baseURL}/movements?type=${MovementType.EXPENSE}`;
+    const req = await fetch(URL);
+    const response = await req.json();
+
+    expect(response.movements.length).toBe(1);
+    expect(firstMovement.id).toBe(response.movements[0].id);
+});
+
+test('Buscar movimientos por api filtrando por tipo inexistente', async () => {
+    const firstMovementData = {
+        date: '01/01/2021',
+        amount: 1000.0,
+        category: 'Supermercado',
+    };
+
+    const secondMovementData = {
+        date: '04/01/2021',
+        amount: 50000.0,
+        type: MovementType.INCOME,
+        category: 'Sueldo',
+    };
+
+    // Creamos los movimientos
+    await MovementModel.create(firstMovementData);
+    await MovementModel.create(secondMovementData);
+
+    const URL = `${baseURL}/movements?type=fake`;
+    const req = await fetch(URL);
+    const response = await req.json();
+
+    expect(response.movements.length).toBe(0);
+});
+
+test('Buscar movimientos por api con más de un resultado', async () => {
+    const firstMovementData = {
+        date: '01/01/2021',
+        amount: 1000.0,
+        category: 'Supermercado',
+    };
+
+    const secondMovementData = {
+        date: '04/01/2021',
+        amount: 50000.0,
+        type: MovementType.INCOME,
+        category: 'Sueldo',
     };
 
     // Creamos los movimientos
