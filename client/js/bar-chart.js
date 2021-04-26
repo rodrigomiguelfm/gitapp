1d0fb69fcfe8cb1d1ebeab6805845de0fcfeeeee
Ivan D'Uva <duva.ivan@outlook.com>
2021-04-26 14:36:36 -0300

    Agrega grafico de barras

---
+++ client/js/bar-chart.js
@@ -0,0 +1,25 @@
+/* global Chart */
+
+function init(labels, data) {
+    const ctx = document.getElementById('month_balance');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: data,
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                }
+            }
+        },
+    });
+}
+
+export default {
+    init,
+};
