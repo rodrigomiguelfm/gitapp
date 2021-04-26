033934fc0ac87179a15732395c974cfda1b15c02
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2021-04-26 10:52:26 -0300

    Agrega eventos con llamadas mock a la api

--- client/js/income.js
+++ client/js/income.js
@@ -1,9 +1,10 @@
-import { getRefs, addListeners, render } from './render.js';
+import { getRefs, render } from './render.js';
 import movementService from './movement-service.js';
 
 let state = {
     movements: [],
-    movement: {}
+    movement: {},
+    hasEdit: true
 };
 
 let refs = getRefs(document.body);
@@ -30,5 +31,49 @@ async function init() {
     renderIncomes(state);
 }
 
+
+// Event Listeners
+
+/**
+ * Agrega un movimiento a edicion
+ **/
+window.editMovement = function (movement) {
+    state.movement = movement;
+    render('movement-form.html', state, refs.form);
+};
+
+/**
+ * Cancela una edicion o creación
+ **/
+window.onCancel = function () {
+    state.movement = {};
+    render('movement-form.html', state, refs.form);
+};
+
+/**
+ * Elimina un movimiento
+ **/
+window.onRemove = async function () {
+    await movementService.update(state.movement);
+    state.movement = {};
+    render('movement-form.html', state, refs.form);
+};
+
+/**
+ * Guarda un movimiento
+ **/
+window.onSave = async function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (state.movement.id) {
+        await movementService.update(state.movement);
+    } else {
+        await movementService.create(state.movement);
+    }
+
+    state.movement = {};
+    render('movement-form.html', state, refs.form);
+};
 
 init();
--- client/js/movement-service.js
+++ client/js/movement-service.js
@@ -6,6 +6,52 @@ async function getLast() {
     return movements;
 }
 
+async function getIncomes(id) {
+    return new Promise(resolve => {
+        resolve([
+            {
+                id: 3,
+                date: '2021-04-01T03:00:00.000Z',
+                amount: 50000,
+                type: 'income',
+                category: 'Sueldo',
+            },
+            {
+                id: 4,
+                date: '2021-05-01T03:00:00.000Z',
+                amount: 10000,
+                type: 'income',
+                category: 'Plazo Fijo',
+            },
+        ]);
+    });
+}
+
+async function update(movement) {
+    console.log('update:', movement);
+    return new Promise(resolve => {
+        resolve();
+    });
+}
+
+async function create(movement) {
+    console.log('create:', movement);
+    return new Promise(resolve => {
+        resolve();
+    });
+}
+
+async function remove(movement) {
+    console.log('delete:', movement);
+    return new Promise(resolve => {
+        resolve();
+    });
+}
+
 export default {
+    create,
+    update,
+    remove,
     getLast,
+    getIncomes,
 };
