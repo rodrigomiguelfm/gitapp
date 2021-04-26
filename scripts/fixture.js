0265256c31fa0f36b70144bd44a8e3b3401ef530
Ivan D'Uva <duva.ivan@outlook.com>
2021-04-26 14:35:57 -0300

    Agrega mas datos al fixture

--- scripts/fixture.js
+++ scripts/fixture.js
@@ -61,6 +61,85 @@ const init = () => {
                 amount: 2500.0,
                 category: 'Reparación Celular',
             })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '14/01/2021',
+                amount: 1432.0,
+                category: 'Supermercado',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '15/01/2021',
+                amount: 2300.0,
+                category: 'Librería',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '20/01/2021',
+                amount: 4877.0,
+                category: 'Regalos',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '21/01/2021',
+                amount: 2900.0,
+                category: 'Regalos',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '04/02/2021',
+                amount: 50000.0,
+                type: MovementType.INCOME,
+                category: 'Sueldo',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '05/02/2021',
+                amount: 11000.0,
+                type: MovementType.INCOME,
+                category: 'Plazo Fijo',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '07/02/2021',
+                amount: 2100.0,
+                category: 'Supermercado',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '08/02/2021',
+                amount: 876.0,
+                category: 'Supermercado',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '10/02/2021',
+                amount: 1398.0,
+                category: 'Supermercado',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '15/02/2021',
+                amount: 3456.0,
+                category: 'Supermercado',
+            })
+        )
+        .then(() =>
+            MovementModel.create({
+                date: '20/02/2021',
+                amount: 1498.0,
+                category: 'Supermercado',
+            })
         );
 };
 
