5e6231197593fb3a0eb8c3501a62f2ec25aff9de
VeronicaMacchi <veronicamacchiaroli@gmail.com>
2021-06-17 01:18:04 +0000

    Se amplió el modelo agregandole el campo description y su respectivo test unitario

--- server/models/movement.js
+++ server/models/movement.js
@@ -30,6 +30,10 @@ const Movement = db.define(
             type: Sequelize.STRING,
             allowNull: false,
         },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
     },
     { tableName: 'Movement' }
 );
@@ -68,10 +72,11 @@ const createMovement = ({
     amount = 0.0,
     type = MovementType.EXPENSE,
     category = '',
+    description = '',
 } = {}) => {
 	/* Comentamos la asignacion de fecha. Esto genera un Bug */
    // date = new Date()
-    return Movement.create({ date, amount, type, category });
+    return Movement.create({ date, amount, type, category, description });
 };
 
 /**
@@ -87,11 +92,12 @@ const updateMovement = (
         amount = 0.0,
         type = MovementType.EXPENSE,
         category = '',
+        description = '',
     } = {}
 ) => {
     return Movement.findOne({ where: { id: id } }).then((movement) => {
         if (movement != null) {
-            return movement.update({ date, amount, type, category });
+            return movement.update({ date, amount, type, category, description });
         }
         return null;
     });
